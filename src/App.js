f20753fd175d20ccb1de99bec974e4f713db9932
Adeyemi <yemiklein77@gmail.com>
2023-01-10 20:19:13 +0100

    features added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import { Feed } from "@mui/icons-material";
-import ChannelDetail from "./components/ChannelDetail";
-import VideoDetail from "./components/VideoDetail";
-import SearchFeed from "./components/SearchFeed";
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backGroundColor: "#000" }}>
+    <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
